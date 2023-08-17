f9cee9c16c2031e1cb2738e437434f39f812b6c1
Sneha <55897319+sneha-afk@users.noreply.github.com>
2023-08-16 20:12:15 -0700

    version 1.0 of databot!

--- databot.py
+++ databot.py
@@ -1,31 +1,103 @@
 import asyncio
 import streamlit as st
-import pandas as pd
-import numpy as np
-import semantic_kernel as sk
-from utils import *
 from typing import List, Union
 
+import semantic_kernel as sk
+from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
+from semantic_kernel.core_skills import DataSkill
 
-variables = dict()
+from utils import get_answer, get_dataframes
+
+ERR_MSG = """Error with query, either I was unable to code the write solution or I couldn't 
+understand your query. Apologies, maybe try rewording your question?
+"""
+
+# Used at runtime
+vars = dict()
+kernel: sk.Kernel
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+def clear_history():
+    st.session_state.messages = []
+
+
+def load_history():
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
 
 # Add new bot message underneath previous content
 def add_message(
-    msg: Union[str, List[str]],
-    role: str = "assistant",
+    msg: Union[str, List[str]], role: str = "assistant", keep_history: bool = False
 ):
     with st.chat_message(role):
         if isinstance(msg, List):
             for m in msg:
                 st.write(m)
+
+            if keep_history:
+                st.session_state.messages.append({"role": role, "content": m})
         else:
             st.write(msg)
 
+            if keep_history:
+                st.session_state.messages.append({"role": role, "content": msg})
 
-def starting():
+
+async def files_and_queries():
+    files = st.file_uploader(
+        "Upload CSV file(s)",
+        accept_multiple_files=True,
+        type="csv",
+        on_change=clear_history,
+    )
+
+    if len(files):
+        dataframes = get_dataframes(files)
+        vars["ds_instance"].add_data(*dataframes)
+
+        add_message("What do you want to know about?")
+
+        load_history()
+        if query := st.chat_input():
+            add_message(query, "user", True)
+
+            answer = ""
+            try:
+                answer = await get_answer(query, vars["query_func"])
+            except:
+                answer = ERR_MSG
+
+            # Known bug, need to fix
+            if answer == query:
+                answer = ERR_MSG
+
+            add_message(answer, "assistant", True)
+
+
+def setup(api_key):
+    chat_service = OpenAIChatCompletion("gpt-3.5-turbo", api_key)
+
+    kernel = sk.Kernel()
+    kernel.add_chat_service("DataBot", chat_service)
+    data_skill_instance = DataSkill(service=chat_service)
+    skill_functions = kernel.import_skill(data_skill_instance, "data")
+    query_func = skill_functions["queryAsync"]
+
+    vars["kernel"] = kernel
+    vars["chat_serv"] = chat_service
+    vars["ds_instance"] = data_skill_instance
+    vars["query_func"] = query_func
+
+
+async def main():
     st.title("DataBot 🤖")
     st.subheader("An AI-powered data analysis tool.")
+    st.caption("Made with :heart: by [sneha-afk](https://github.com/sneha-afk)")
 
     add_message(
         [
@@ -40,37 +112,11 @@ def starting():
         help="Visit OpenAI to obtain an API key!",
         placeholder="abc-...",
     )
-    return api_key_input
 
-
-async def main():
-    if api_key := starting():
-        add_message(
-            "Great! Now upload CSV data for me to analyze along with your query."
-        )
-        files = st.file_uploader(
-            "Upload CSV file(s)", accept_multiple_files=True, type="csv"
-        )
-
-        if len(files):
-            dataframes = get_dataframes(files)
-            query_func = setup(api_key, dataframes)
-
-            add_message("And what would you like to know about this data?")
-
-            # query = st.chat_input()
-            # if query:
-            #     add_message(query, "user")
-            # answer = await query_func.invoke_async(query)
-            # add_message(answer)
-
-            query = st.text_area(
-                "Query for data", placeholder="What is the average value of..."
-            )
-
-            if st.button("Submit"):
-                answer = await get_answer(query, query_func)
-                add_message(answer)
+    if api_key := api_key_input:
+        add_message("Great! Please upload some CSV data for me to analyze!")
+        setup(api_key)
+        await files_and_queries()
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,26 +1,6 @@
 import semantic_kernel as sk
-from semantic_kernel import Kernel
-from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
-from semantic_kernel.connectors.ai import ChatCompletionClientBase
-from semantic_kernel.core_skills import DataSkill
-from typing import Any, List, Union
+from typing import Any, List
 import pandas as pd
-import streamlit as st
-
-
-def setup(api_key: str, user_data) -> Kernel:
-    openai_chat_completion = OpenAIChatCompletion("gpt-3.5-turbo", api_key)
-
-    kernel = Kernel()
-    kernel.add_chat_service("DataBot", openai_chat_completion)
-
-    data_skill = kernel.import_skill(
-        DataSkill(data=user_data, service=openai_chat_completion),
-        skill_name="data",
-    )
-    query_async = data_skill["queryAsync"]
-
-    return query_async
 
 
 def get_dataframes(data: List[Any]):
